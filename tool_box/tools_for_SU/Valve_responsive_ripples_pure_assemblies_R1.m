97aed6ebf28de852d5c0f3d6b283f9863679623e
facuumm <89267665+facuumm@users.noreply.github.com>
2025-07-25 18:01:02 +0200

    revisions

--- tool_box/tools_for_SU/Valve_responsive_ripples_pure_assemblies_R1.m
+++ tool_box/tools_for_SU/Valve_responsive_ripples_pure_assemblies_R1.m
@@ -1,4 +1,4 @@
-function [Pre Post T I curve] = Valve_responsive_ripples(path) 
+function [Pre Post T I curve] = Valve_responsive_ripples_pure_assemblies_R1(path)
 % This function calculates the Pre and Post sleep assemblies rate.
 %
 % INPUTS
@@ -57,31 +57,6 @@ for tt = 1:length(path)
         %Loading TS of the sessions
         disp('Uploading session time stamps')
         load('session_organization.mat')
-        load('behavioral_dataVF.mat')
-
-        %% speed sourrounding the shocks
-        means = meanInGroups(behavior.speed.reward(:,2), 3);
-        downsampled_t = downsampleTimeVector(behavior.speed.reward(:,1), 1/30, 0.1);
-        
-        if size(means,2) < size(downsampled_t,2)
-            means = [downsampled_t(1:size(means,2))' , means']; clear downsampled_t
-        elseif size(means,2) > size(downsampled_t,2)
-            means = [downsampled_t' , means(1:size(downsampled_t,2))']; clear downsampled_t
-        else
-            means = [downsampled_t' , means']; clear downsampled_t
-        end
-        
-        tmp = [];
-        for i = 1 : length(Rewards_filt)
-            [~ , ii] = min(abs(means(:,1)-Rewards_filt(i,1)));
-            if and(ii+50 < length(means) , ii>50)
-                tmp = [tmp , means(ii-50 : ii+50 , 2)];
-            end
-        end
-        Mean = nanmean(tmp'); clear tmp means
-        curve.speed.data = [curve.speed.data , Mean']; clear Mean
-        curve.speed.id = [curve.speed.id ; tt t length(Rewards_filt)];
-        
         
         %% load sleep states
         disp('Uploading sleep scoring')
@@ -129,7 +104,7 @@ for tt = 1:length(path)
                     low = min([r(1) , z(indice,1)]);
                     up = max([r(3) , z(indice,3)]);
                     cooridnated_event = [cooridnated_event ; low , peak , up];
-
+                    
                     clear tmp2 tmp1 p indice z peak low up
                 end
                 clear r
@@ -140,13 +115,32 @@ for tt = 1:length(path)
             coordinatedV  = coordinatedV(IA,:); clear C IA IC
             
             % Store events time stamps
+            % dRipples
+            ripples.dHPC.baseline = Restrict(ripplesD , NREM.baseline);
+            ripples.dHPC.reward = Restrict(ripplesD , NREM.reward);
+            ripples.dHPC.aversive = Restrict(ripplesD , NREM.aversive);
+            % vRipples
+            ripples.vHPC.baseline = Restrict(ripplesV , NREM.baseline);
+            ripples.vHPC.reward = Restrict(ripplesV , NREM.reward);
+            ripples.vHPC.aversive = Restrict(ripplesV , NREM.aversive);
             % coordinated dRipples
             ripples.dHPC.coordinated.all = coordinated;
             ripples.dHPC.uncoordinated.all = ripplesD(not(ismember(ripplesD(:,2) , coordinated(:,2))),:);
+            ripples.dHPC.coordinated.baseline = Restrict(coordinated , NREM.baseline);
+            ripples.dHPC.coordinated.reward = Restrict(coordinated , NREM.reward);
+            ripples.dHPC.coordinated.aversive = Restrict(coordinated , NREM.aversive);
             % coordinated vRipples
             ripples.vHPC.coordinated.all = coordinatedV;
             ripples.vHPC.uncoordinated.all = ripplesV(not(ismember(ripplesV(:,2) , coordinatedV(:,2))),:);
-
+            ripples.vHPC.coordinated.baseline = Restrict(coordinatedV , NREM.baseline);
+            ripples.vHPC.coordinated.reward = Restrict(coordinatedV , NREM.reward);
+            ripples.vHPC.coordinated.aversive = Restrict(coordinatedV , NREM.aversive);
+            %coordinated event
+            cooridnated_event((cooridnated_event(:,3)-cooridnated_event(:,1)<0.04),:) = [];
+            ripple_event.baseline = Restrict(cooridnated_event,baselineTS);
+            ripple_event.reward = Restrict(cooridnated_event,rewardTS);
+            ripple_event.aversive = Restrict(cooridnated_event,aversiveTS);
+            ripple_event.all = cooridnated_event;
         else
             RB = false;
         end
@@ -160,7 +154,7 @@ for tt = 1:length(path)
         if isfile('dorsalventral_assemblies_rewardVF.mat')
             disp('Loading Reward template')
             [patterns , cond , Thresholded] = load_assemblies(cd , 'dorsalventral_assemblies_rewardVF.mat', clusters, numberD , 'reward');
-            th = sum(Thresholded.reward(:,cond.both),2)>=1;
+            th = sum(Thresholded.reward(:,not(cond.both)),2)>=1;
         else
             th = zeros(size(clusters.all,1),1);
         end
@@ -180,28 +174,23 @@ for tt = 1:length(path)
                         end
                     else
                         if aversiveTS_run(1)<rewardTS_run(1)
-                            TS.pre = NREM.aversive;        TS.post = NREM.reward;                    
+                            TS.pre = Restrict(NREM.aversive,[NREM.aversive(end,2)-3600 NREM.aversive(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
                         else
-                            TS.pre = NREM.baseline;        TS.post = NREM.reward;
+                            TS.pre = Restrict(NREM.baseline,[NREM.baseline(end,2)-3600 NREM.baseline(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
                         end
-%                         if aversiveTS_run(1)<rewardTS_run(1)
-%                             TS.pre = Restrict(NREM.aversive,[NREM.aversive(end,2)-3600 NREM.aversive(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
-%                         else
-%                             TS.pre = Restrict(NREM.baseline,[NREM.baseline(end,2)-3600 NREM.baseline(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
-%                         end
                     end
                     
                     for ii = 1 : numberD
                         % Pre
                         y = Restrict(spks(spks(:,1)==clusters.dHPC(ii),2),TS.pre);
                         x = Restrict(ripples.dHPC.uncoordinated.all(:,1:3),TS.pre);
-%                         x = Restrict(ripplesD(:,1:3),TS.pre);
                         
                         if and(size(x,1)>5 , size(y,1)>5)
                             % Check to keep only ripples that are within the Times2
                             tmp = and(x(:,2) > y(1), x(:,2) < y(end));
                             x = x(tmp,:); clear tmp
-                            
+                            %                             [m] = meanFR_outside_ripples(ripplesD(:,1:3) , TS.pre , y , bin);
+                            %                             [m] = meanFR_outside_ripples(ripplesD(:,1:3) , NREM.all , y);
                             [s,ids,groups] = CCGParameters(x(:,2),ones(length(x(:,2)),1),y,ones(length(y),1)*2);
                             [ccg1,T] = CCG(s,ids,'binSize',bin,'duration',dur,'smooth',sm,'mode','ccg');
                             ccg1 = ccg1(:,1,2)./size(x,1);    ccg1 = ccg1./bin; %ccg1 = ccg1./m;
@@ -213,7 +202,7 @@ for tt = 1:length(path)
                         % Post
                         y = Restrict(spks(spks(:,1)==clusters.dHPC(ii),2),TS.post);
                         x = Restrict(ripples.dHPC.uncoordinated.all(:,1:3),TS.post);
-
+                        
                         if and(size(x,1)>5 , size(y,1)>5)
                             % Check to keep only ripples that are within the Times2
                             tmp = and(x(:,2) > y(1), x(:,2) < y(end));
@@ -261,22 +250,17 @@ for tt = 1:length(path)
                         end
                     else
                         if aversiveTS_run(1)<rewardTS_run(1)
-                            TS.pre = NREM.aversive;        TS.post = NREM.reward;                       
+                            TS.pre = NREM.aversive;        TS.post = NREM.reward;
                         else
                             TS.pre = NREM.baseline;        TS.post = NREM.reward;
                         end
-%                         if aversiveTS_run(1)<rewardTS_run(1)
-%                             TS.pre = Restrict(NREM.aversive,[NREM.aversive(end,2)-3600 NREM.aversive(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
-%                         else
-%                             TS.pre = Restrict(NREM.baseline,[NREM.baseline(end,2)-3600 NREM.baseline(end,2)]);        TS.post = Restrict(NREM.reward,[NREM.reward(1,1) NREM.reward(1,1)+3600]);
-%                         end
                     end
                     
                     for ii = 1 : numberV
                         % Pre
                         y = Restrict(spks(spks(:,1)==clusters.vHPC(ii),2),TS.pre);
                         x = Restrict(ripples.vHPC.uncoordinated.all(:,1:3),TS.pre);
-
+                        
                         if and(size(x,1)>5 , size(y,1)>5)
                             % Check to keep only ripples that are within the Times2
                             tmp = and(x(:,2) > y(1), x(:,2) < y(end));
@@ -298,7 +282,6 @@ for tt = 1:length(path)
                             % Check to keep only ripples that are within the Times2
                             tmp = and(x(:,2) > y(1), x(:,2) < y(end));
                             x = x(tmp,:); clear tmp
-                            
                             [s,ids,groups] = CCGParameters(x(:,2),ones(length(x(:,2)),1),y,ones(length(y),1)*2);
                             [ccg2,T] = CCG(s,ids,'binSize',bin,'duration',dur,'smooth',sm,'mode','ccg');
                             ccg2 = ccg2(:,1,2)./size(x,1);    ccg2 = ccg2./bin; %ccg2 = ccg2./m;
@@ -312,7 +295,7 @@ for tt = 1:length(path)
                                 Pre.aversive.vHPC = [Pre.aversive.vHPC , ccg1];
                                 Post.aversive.vHPC = [Post.aversive.vHPC , ccg2];
                                 
-                                I.vHPC = [I.vHPC ; vHPC_valve.responssiveness(vHPC_valve.id == clusters.vHPC(ii)) clusters.vHPC(ii) t tt th(ii)]; clear p
+                                I.vHPC = [I.vHPC ; vHPC_valve.responssiveness(vHPC_valve.id == clusters.vHPC(ii)) clusters.vHPC(ii) t tt th(ii+numberD)]; clear p
                             else
                                 Pre.reward.vHPC = [Pre.reward.vHPC , ccg1];
                                 Post.reward.vHPC = [Post.reward.vHPC , ccg2];
